672d97422cc0cb773e2efefb2f4f62ad164d4b75
albuska <109683293+albuska@users.noreply.github.com>
2023-04-03 21:38:15 +0300

    correct

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import { GlobalStyle } from '../GlobalStyle';
+import {Container} from './App.styled'; 
+import { Component } from 'react';
+
+export class App extends Component {
+ 
+  render() {
+    return (
+      <div>
+        React homework template
+      </div>
+    );
+  }
+};
